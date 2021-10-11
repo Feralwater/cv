2432300ccf237d0d8878e428743476803bff3315
Feralwater <lineviktoriya@gmail.com>
2021-10-11 20:13:01 +0300

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import s from "./App.module.scss"
 import Navbar from "./navBar/Navbar";
+import Home from "./home/Home";
 
 function App() {
     return (
         <div className={s.App}>
             <Navbar/>
+            <Home/>
         </div>
     );
 }
