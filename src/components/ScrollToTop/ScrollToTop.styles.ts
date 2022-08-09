90a3ffa5f928fed2fabfd8e0c2f05a7bf4e641b4
victoria.buyakova <lineviktoriya@gmail.com>
2022-08-09 18:33:04 +0400

    fis scroll to top button styles

--- src/components/ScrollToTop/ScrollToTop.styles.ts
+++ src/components/ScrollToTop/ScrollToTop.styles.ts
@@ -8,11 +8,14 @@ export const ScrollToTopButton = styled.button<IScrollToTopButton>`
   position: fixed;
   right: 15px;
   bottom: 30px;
-  background-color: #eeeeee;
-  padding: 15px;
-  border-radius: 3px;
+  background-color: ${({ theme }) => theme.colors.font};
+  padding: 10px 12px;
+  border-radius: 4px;
   display: inline-block;
   overflow: hidden;
-  cursor: none;
+  cursor: pointer;
   opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+  color: ${({ theme }) => theme.colors.sidebarBackground};
+  font-size: 20px;
+  border: none;
 `;
