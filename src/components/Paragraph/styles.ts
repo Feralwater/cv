6cdcefff9cbb8bfe35340da050c856a31c628f0e
victoria.buyakova <lineviktoriya@gmail.com>
2022-08-21 23:44:42 +0400

    add media queries to paragraph

--- src/components/Paragraph/styles.ts
+++ src/components/Paragraph/styles.ts
@@ -4,4 +4,8 @@ export const StyledParagraph = styled.p`
   color: ${({ theme }) => theme.colors.paragraph};
   font-size: 18px;
   line-height: 1.5;
+
+  @media (max-width: ${({ theme }) => theme.media.medium}) {
+    font-size: 16px;
+  }
 `;
