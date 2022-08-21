5e7070096f51fe42b459e9abbddd44a3a8fa8d77
victoria.buyakova <lineviktoriya@gmail.com>
2022-08-21 23:44:10 +0400

    add media queries to page info

--- src/components/PageInfo/styles.ts
+++ src/components/PageInfo/styles.ts
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 export const Info = styled.div`
   width: 50%;
+
+  @media (max-width: ${({ theme }) => theme.media.medium}) {
+    width: 100%;
+  }
 `;
 
 export const H3 = styled.h3`
@@ -10,4 +14,8 @@ export const H3 = styled.h3`
   font-weight: 700;
   line-height: 1;
   margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.media.small}) {
+    font-size: 35px;
+  }
 `;
